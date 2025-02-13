7c61b2d3c0cd8ac2f1f238f9463747a203d54523
Microsoft.com Team <ramesh.anantavarapudev@gmail.com>
2025-02-13 22:00:37 +0530

    Dockerfile&controller

---
+++ src/main/java/com/example/spring/boot/demo/Helloworld.java
@@ -0,0 +1,12 @@
+package com.example.spring.boot.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+    @GetMapping
+    public String home(){
+        return "Welcome to Spring Boot";
+    }
+}
